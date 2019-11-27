3ff25772901e1002c02ef706afbca9b449dfbeaa
Guillaume Roux <guillaumeroux123@gmail.com>
2019-11-27 23:17:55 +0100

    continued filter placement

--- js/webcam.js
+++ js/webcam.js
@@ -1,4 +1,5 @@
 var constraints = { video:{width: 720, height: 720}, audio: false };
+var mouseDown = false;
 
 const	cameraView = document.querySelector('#camera-view'),
 		cameraSensor = document.querySelector('#camera-sensor'),
@@ -22,6 +23,16 @@ function initFilters() {
 	filterScreen.height = constraints.video.height;
 }
 
+var moveFilter = (e) => {
+	if (mouseDown)
+	{
+		ctx = filterScreen.getContext('2d');
+		var rect = e.target.getBoundingClientRect();
+		ctx.clearRect(0, 0, filterScreen.width, filterScreen.height);
+		ctx.fillRect((e.clientX - rect.left - 125), (e.clientY - rect.top - 125), 250, 250)
+	}
+}
+
 cameraBtn.onclick = (e) => {
 	cameraSensor.width = cameraView.videoWidth;
 	cameraSensor.height= cameraView.videoHeight;
@@ -29,12 +40,10 @@ cameraBtn.onclick = (e) => {
 	console.log(cameraSensor.toDataURL('image/png'));
 }
 
-filterScreen.addEventListener('mousemove', (e) => {
-	ctx = filterScreen.getContext('2d');
-	var rect = e.target.getBoundingClientRect();
-	ctx.clearRect(0, 0, filterScreen.width, filterScreen.height);
-	ctx.fillRect((e.clientX - rect.left), (e.clientY - rect.top), 250, 250)
-});
+filterScreen.addEventListener('mousedown', (e) => {mouseDown = true});
+filterScreen.addEventListener('mouseup', (e) => {mouseDown = false});
+filterScreen.addEventListener('mouseover', moveFilter, false);
+filterScreen.addEventListener('mousemove', moveFilter, false);
 
 window.onload = () => {
 	cameraStart();
